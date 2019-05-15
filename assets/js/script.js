2fd1a96eb5243281a072de6763c3a79f6ff1c61c
Victoria <vdelsanted@gmail.com>
2019-05-15 19:22:00 -0400

    prueba4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,5 @@
 $(function() {
 
-  $("a").click(function(event){
-      if (this.hash !== "") {
-        event.preventDefault();
-
-        var gato = this.hash;
-
-        $("html, body").animate({
-          scrollTop: $(gato).offset().top
-        }, 800, function(){
-          window.location.hash = gato;
-        });
-      }
-    });
 
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover();
